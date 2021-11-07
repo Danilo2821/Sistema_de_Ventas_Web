13b7e662a844ab733fea69c960de577df043c8c6
Danilo Puertas <89751823+Danilo2821@users.noreply.github.com>
2021-11-07 02:00:22 -0300

    Se modificaron las clases de Cliente para crear las entidades y se agregaron los metodos del CRUD a la clase java de ClienteDAO

--- SistemaVentasWeb1/src/java/Modelo/ClienteDAO.java
+++ SistemaVentasWeb1/src/java/Modelo/ClienteDAO.java
@@ -5,10 +5,136 @@
  */
 package Modelo;
 
+import config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
- * @author Danim
+ * @author Dani
  */
 public class ClienteDAO {
-    
+
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    int r;
+
+    public Cliente buscar(String dni) {
+        Cliente cl = new Cliente();
+        String sql = "select * from cliente where Dni=" + dni; //dni que viene como parametro
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                cl.setId(rs.getInt(1));//se usa la var del obj cliente con el metodo set para obtener los datos de los camp y guardarlos en el obj
+                cl.setDni(rs.getString(2));
+                cl.setNom(rs.getString(3));
+                cl.setDir(rs.getString(4));
+                cl.setEstado(rs.getString(5));
+            }
+        } catch (Exception e) {
+
+        }
+        return cl;//este metodo va a retornar el cliente encontrado en nuestra BD
+    }
+
+    //Operaciones CRUD
+    public List listar() {
+        String sql = "select * from cliente";
+        // la variable list guarda el objeto empleado y lo instancia como un array
+        List<Cliente> clientes = new ArrayList<>();
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Cliente cl = new Cliente(); //Se instancia el obj empleado
+                cl.setId(rs.getInt(1)); //Se utiliza el metodo set para obtener los valores de los campos y guardarlos en el objeto em
+                cl.setDni(rs.getString(2));
+                cl.setNom(rs.getString(3));
+                cl.setDir(rs.getString(4));
+                cl.setEstado(rs.getString(5));// todo el objeto se agrega a la variable lista
+                clientes.add(cl);
+            }
+        } catch (Exception e) {
+
+        }
+        return clientes;
+    }
+
+    // el metodo agregar tiene como paramentro el objeto emplado
+    public int agregar(Cliente cl) {
+        String sql = "insert into cliente(Dni,Nombres,Direccion,Estado)values(?,?,?,?)";
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cl.getDni());
+            ps.setString(2, cl.getNom());
+            ps.setString(3, cl.getDir());
+            ps.setString(4, cl.getEstado());
+            ps.executeUpdate();//Con este metodo se van a agregar los datos a la BD
+
+        } catch (Exception e) {
+
+        }
+        return r;
+    }
+
+    public Cliente listarId(int id) {
+        Cliente cli = new Cliente();
+        String sql = "select * from cliente where IdCliente=" + id;
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                cli.setDni(rs.getString(2));
+                cli.setNom(rs.getString(3));
+                cli.setDir(rs.getString(4));
+                cli.setEstado(rs.getString(5));
+            }
+        } catch (Exception e) {
+
+        }
+        return cli;
+    }
+
+    public int actualizar(Cliente cl) {
+        String sql = "update empleado set Dni=?, Nombres=?, Direccion=?, Estado=? where idCliente=?";
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cl.getDni());
+            ps.setString(2, cl.getNom());
+            ps.setString(3, cl.getDir());
+            ps.setString(4, cl.getEstado());
+            ps.setInt(5, cl.getId());
+            ps.executeUpdate();//Con este metodo se van a agregar los datos a la BD
+
+        } catch (Exception e) {
+
+        }
+        return r;
+
+    }
+
+    // el metodo delete tiene como paramentro el id del objeto
+    public void delete(int id) {
+        String sql = "delete from cliente where IdCliente=" + id;
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.executeUpdate();// Este metodo va a elimninar al usuario que sea seleccionado de nuestra BD
+        } catch (Exception e) {
+
+        }
+
+    }
+
 }
