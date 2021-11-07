1eab1b73089a9ca79d5e880a29610e06c5efb5b6
Danilo Puertas <89751823+Danilo2821@users.noreply.github.com>
2021-11-07 01:28:58 -0300

    Modificaciones en las clases Empleado (se agregaron los elementos para crear la entidad), EmpleadoDAO (se crearon los metodos de validar usuario) y Conexion (se creo la conexion a la BD) Modificaciones en el servlet Validar (Se configuraron los metodos de validacion de usuario que se realizan en EmpleadoDAO)

--- SistemaVentasWeb1/src/java/Controlador/Validar.java
+++ SistemaVentasWeb1/src/java/Controlador/Validar.java
@@ -5,6 +5,8 @@
  */
 package Controlador;
 
+import Modelo.Empleado;
+import Modelo.EmpleadoDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -27,6 +29,10 @@ public class Validar extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    EmpleadoDAO edao=new EmpleadoDAO();
+    Empleado em=new Empleado();
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -70,7 +76,20 @@ public class Validar extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       String accion=request.getParameter("accion");
+       if(accion.equalsIgnoreCase("Ingresar")){
+           String user=request.getParameter("txtuser");
+           String pass=request.getParameter("txtpass");
+           em=edao.validar(user, pass);
+           if(em.getUser()!=null){
+               request.getRequestDispatcher("Controlador?accion=Principal").forward(request, response);
+           }else{
+               request.getRequestDispatcher("index.jsp").forward(request, response);
+           }   
+       }
+       else{
+          request.getRequestDispatcher("index.jsp").forward(request, response); 
+       }
     }
 
     /**
--- SistemaVentasWeb1/src/java/config/Conexion.java
+++ SistemaVentasWeb1/src/java/config/Conexion.java
@@ -5,10 +5,28 @@
  */
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Danim
  */
 public class Conexion {
+    
+    Connection con;
+    String url="jdbc:mysql://localhost:3306/bd_ventas";
+    String user="root";
+    String pass="root";
+   
+    public Connection Conexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection(url, user, pass);
+            
+        }catch(Exception e){
+        }
+        return con;
+    }
     
 }
